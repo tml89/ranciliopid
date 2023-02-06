a24da2cb1104fe33ad9a28ea844019acde76be6b
tml89 <schmid.tim@pm.me>
2023-02-06 19:30:12 +0100

    Add deepSleep

--- src/userConfig_sample.h
+++ src/userConfig_sample.h
@@ -1,15 +1,15 @@
 /**
  * @file    userConfig_sample.h
  * @brief   Values must be configured by the user
- * @version 3.1.0 Master 
+ * @version 4.0.0 Master
  *
  */
-#ifndef _userConfig_H
-#define _userConfig_H
+
+#pragma once
 
 // firmware version (must match with definitions in the main source file)
-#define USR_FW_VERSION    3
-#define USR_FW_SUBVERSION 1
+#define USR_FW_VERSION    4
+#define USR_FW_SUBVERSION 0
 #define USR_FW_HOTFIX     0
 #define USR_FW_BRANCH     "MASTER"
 
@@ -33,14 +33,28 @@ enum MACHINE {
 #define OLED_I2C 0x3C              // I2C address for OLED, 0x3C by default
 #define DISPLAYTEMPLATE 3          // 1 = Standard display template, 2 = Minimal template, 3 = only temperature, 4 = scale template, 20 = vertical display (see git Handbook for further information)
 #define DISPLAYROTATE U8G2_R0      // rotate display clockwise: U8G2_R0 = no rotation; U8G2_R1 = 90°; U8G2_R2 = 180°; U8G2_R3 = 270°
+#define SCREEN_WIDTH 128           // OLED display width, in pixels
+#define SCREEN_HEIGHT 64           // OLED display height, in pixels
 #define SHOTTIMER 1                // 0 = deactivated, 1 = activated 2 = with scale
 #define HEATINGLOGO 0              // 0 = deactivated, 1 = Rancilio, 2 = Gaggia
 #define OFFLINEGLOGO 1             // 0 = deactivated, 1 = activated
 #define BREWSWITCHDELAY 3000       // time in ms that the brew switch will be delayed (shot timer will show that much longer after switching off)
 #define VERBOSE 0                  // 1 = Show verbose output (serial connection), 0 = show less
 
+#define LANGUAGE 0                 // LANGUAGE = 0 (DE), LANGUAGE = 1 (EN), LANGUAGE = 2 (ES)
+
+// NTP
+#define NTP_SERVER "de.pool.ntp.org"
+#define TZ_INFO "WEST-1DWEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00" // Western European Time
+
+// DeepSleep Config
+#define SLEEPNIGHT 1
+#define uS_TO_S_FACTOR 1000000ULL
+const unsigned int dndStartH = 21;
+const unsigned int dndEndH = 5;
+
 // Connectivity
-#define CONNECTMODE 1              // 0 = offline 1 = WIFI-MODE 2 = AP-MODE (not working in the moment)
+#define CONNECTMODE 1              // 0 = offline 1 = WIFI-MODE
 #define HOSTNAME "silvia"
 #define PASS "CleverCoffee"        // default password for WiFiManager
 #define MAXWIFIRECONNECTS 5        // maximum number of reconnection attempts, use -1 to deactivate
@@ -52,23 +66,12 @@ enum MACHINE {
 #define BREWMODE 1                 // 1 = Brew by time (with preinfusion); 2 = Brew by weight (from scale)
 #define BREWDETECTION 0            // 0 = off, 1 = Software (Onlypid 1), 2 = Hardware (Onlypid 0), 3 = Sensor/Hardware for Only PID
 #define BREWSWITCHTYPE 1           // 1 = normal Switch, 2 = Trigger Switch
-#define TRIGGERTYPE HIGH           // LOW = low trigger, HIGH = high trigger relay
+#define POWERSWITCHTYPE 0          // 0 = no switch connected, 1 = normal Switch, 2 = Trigger Switch
+#define TRIGGERTYPE HIGH           // LOW = low trigger, HIGH = high trigger relay for pump & valve
 #define VOLTAGESENSORTYPE HIGH     // BREWDETECTION 3 configuration
 #define PINMODEVOLTAGESENSOR INPUT // Mode INPUT_PULLUP, INPUT or INPUT_PULLDOWN_16 (Only Pin 16)
 #define PRESSURESENSOR 0           // 1 = pressure sensor connected to A0; PINBREWSWITCH must be set to the connected input!
-#define TEMPLED 2                  // 1 = set led pin high when brew or steam set point is within range otherwise low; 2 = vice versa
-
-// TOF sensor for water level
-#define TOF 0                      // 0 = no TOF sensor connected; 1 = water level by TOF sensor
-#define TOF_I2C 0x29               // I2C address of TOF sensor; 0x29 by default
-#define CALIBRATION_MODE 0         // 1 = enable to obtain water level calibration values; 0 = disable for normal PID operation; can also be done in Blynk
-#define WATER_FULL 102             // value for full water tank (=100%) obtained in calibration procedure (in mm); can also be set in Blynk
-#define WATER_EMPTY 205            // value for empty water tank (=0%) obtained in calibration procedure (in mm); can also be set in Blynk
-
-// E-Trigger
-#define ETRIGGER 0                 // 0 = no trigger (for Rancilio except Rancilio E), 1 = trigger for CPU of Rancilio E
-#define ETRIGGERTIME 600           // seconds, time between trigger signal
-#define TRIGGERRELAYTYPE HIGH      // LOW = low trigger, HIGH = high trigger relay for E-Trigger
+#define STATUSLED 0                // 1 = set led pin high when brew or steam set point is within range otherwise low; 2 = vice versa
 
 // Brew Scale
 #define SCALE_SAMPLES 2                     // Load cell sample rate
@@ -96,15 +99,6 @@ enum MACHINE {
 #define MQTT_SERVER_IP "XXX.XXX.XXX.XXX"   // IP-Address of locally installed mqtt server
 #define MQTT_SERVER_PORT 1883
 
-// BLYNK
-#define BLYNK 0                                 // 0 = no Blynk , 1 = Blynk
-#define AUTH "blynk_auth"
-#define BLYNKADDRESS "blynk.clevercoffee.de"    // blynk-server IP-Address
-#define BLYNKPORT 8080                          // blynk-server portver
-
-// Grafana
-#define GRAFANA 0                  // 2 = custom Grafana 1 = Grafana visualisation (access required), 0 = off (default)
-
 // INFLUXDB
 #define INFLUXDB 0                 // 1 = INFLUX enabled, 0 = INFLUX disabled
 #define INFLUXDB_URL ""            // InfluxDB server address
@@ -120,39 +114,4 @@ enum MACHINE {
 #define EMA_FACTOR 0.6             // Smoothing of input that is used for Tv (derivative component of PID). Smaller means less smoothing but also less delay, 0 means no filtering
 #define BREWPID_DELAY 10           // delay until enabling PID controller during brew (no heating during this time)
 
-// Backflush values
-#define FILLTIME 3000              // time in ms the pump is running
-#define FLUSHTIME 6000             // time in ms the 3-way valve is open -> backflush
-#define MAXFLUSHCYCLES 5           // number of cycles the backflush should run, 0 = disabled
-
 #define TEMPSENSOR 2               // Temp sensor type: 1 = DS18B20, 2 = TSIC306
-
-// Pin Layout
-#define PINTEMPSENSOR 2            // Temp sensor pin
-#define PINPRESSURESENSOR 99       // Pressuresensor 0: A0 (ESP8266), >0 ONLY ESP32
-#define PINVALVE 12                // Output pin for 3-way-valve
-#define PINPUMP 13                 // Output pin for pump
-#define PINHEATER 14               // Output pin for heater
-#define PINVOLTAGESENSOR 15        // Input pin for voltage sensor (optocoupler to detect brew switch)
-#define PINETRIGGER 16             // PIN for E-Trigger relay
-#define PINBREWSWITCH 0            // 0: A0 (ESP8266) ; >0 : DIGITAL PIN, ESP32 OR ESP8266: ONLY USE PIN15 AND PIN16!
-#define PINSTEAMSWITCH 17          // STEAM active
-#define LEDPIN    18               // LED PIN ON near setpoint
-#define OLED_SCL 5                 // Output pin for display clock pin
-#define OLED_SDA 4                 // Output pin for display data pin
-#define HXDATPIN 99                // weight scale PIN
-#define HXCLKPIN 99                // weight scale PIN
-#define SCREEN_WIDTH 128           // OLED display width, in pixels
-#define SCREEN_HEIGHT 64           // OLED display height, in pixels
-
-// Check BrewSwitch
-#if (defined(ESP8266) && ((PINBREWSWITCH != 15 && PINBREWSWITCH != 0 && PINBREWSWITCH != 16 )))
-  #error("WRONG Brewswitch PIN for ESP8266, Only PIN 15 and PIN 16");
-#endif
-
-// defined compiler errors
-#if (PRESSURESENSOR == 1) && (PINPRESSURESENSOR == 0) && (PINBREWSWITCH == 0)
-  #error Change PINBREWSWITCH or PRESSURESENSOR!
-#endif
-
-#endif
