832429127f39c9ee4448ab665231f96346d49c87
tml89 <schmid.tim@pm.me>
2024-02-16 15:43:45 +0100

    - Push userConfig to backup - make LED handling more rebust and less blocking

---
+++ src/userConfig.h
@@ -0,0 +1,125 @@
+/**
+ * @file    userConfig_sample.h
+ * @brief   Values must be configured by the user
+ * @version 4.0.0 Master
+ *
+ */
+
+#pragma once
+
+// firmware version (must match with definitions in the main source file)
+#define USR_FW_VERSION    4
+#define USR_FW_SUBVERSION 0
+#define USR_FW_HOTFIX     0
+#define USR_FW_BRANCH     "MASTER"
+
+// List of supported machines
+enum MACHINE {
+    RancilioSilvia,   // MACHINEID 0
+    RancilioSilviaE,  // MACHINEID 1
+    Gaggia,           // MACHINEID 2
+    QuickMill         // MACHINEID 3
+};
+
+/**
+ * Preconfiguration
+ */
+
+// Machine
+#define MACHINEID 1                // see above list of supported machines
+
+// Display
+#define OLED_DISPLAY 0             // 0 = deactivated, 1 = SH1106 (e.g. 1.3 "128x64), 2 = SSD1306 (e.g. 0.96" 128x64)
+#define OLED_I2C 0x3C              // I2C address for OLED, 0x3C by default
+#define DISPLAYTEMPLATE 3          // 1 = Standard display template, 2 = Minimal template, 3 = only temperature, 4 = scale template, 20 = vertical display (see git Handbook for further information)
+#define DISPLAYROTATE U8G2_R0      // rotate display clockwise: U8G2_R0 = no rotation; U8G2_R1 = 90°; U8G2_R2 = 180°; U8G2_R3 = 270°
+#define SCREEN_WIDTH 128           // OLED display width, in pixels
+#define SCREEN_HEIGHT 64           // OLED display height, in pixels
+#define SHOTTIMER 1                // 0 = deactivated, 1 = activated 2 = with scale
+#define HEATINGLOGO 0              // 0 = deactivated, 1 = Rancilio, 2 = Gaggia
+#define OFFLINEGLOGO 1             // 0 = deactivated, 1 = activated
+#define BREWSWITCHDELAY 3000       // time in ms that the brew switch will be delayed (shot timer will show that much longer after switching off)
+#define VERBOSE 0                  // 1 = Show verbose output (serial connection), 0 = show less
+
+#define LANGUAGE 0                 // LANGUAGE = 0 (DE), LANGUAGE = 1 (EN), LANGUAGE = 2 (ES
+
+// NTP
+#define NTP_SERVER "de.pool.ntp.org"
+#define TZ_INFO "WEST-1DWEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00" // Western European Time
+
+// DeepSleep Config
+#define SLEEPNIGHT 0   
+#define uS_TO_S_FACTOR 1000000ULL
+const unsigned int dndStartH = 23;
+const unsigned int dndEndH = 5;
+
+// Standby Timer
+#define MaxInactivityTime 30     // max inactivity time in minutes
+
+// Connectivity
+#define CONNECTMODE 1              // 0 = offline 1 = WIFI-MODE
+#define HOSTNAME "silvia"
+#define PASS "CleverCoffee"        // default password for WiFiManager
+#define MAXWIFIRECONNECTS -1        // maximum number of reconnection attempts, use -1 to deactivate
+#define WIFICONNECTIONDELAY 10000  // delay between reconnects in ms
+
+// PID & Hardware
+#define ONLYPID 0                  // 1 = Only PID, 0 = PID and preinfusion
+#define ONLYPIDSCALE 0             // 0 = off , 1 = OnlyPID with Scale
+#define BREWMODE 1                 // 1 = Brew by time (with preinfusion); 2 = Brew by weight (from scale)
+#define BREWDETECTION 2            // 0 = off, 1 = Software (Onlypid 1), 2 = Hardware (Onlypid 0), 3 = Sensor/Hardware for Only PID
+#define BREWSWITCHTYPE 2           // 1 = normal Switch, 2 = Trigger Switch
+#define POWERSWITCHTYPE 2          // 0 = no switch connected, 1 = normal Switch, 2 = Trigger Switch
+#define TRIGGERTYPE HIGH           // LOW = low trigger, HIGH = high trigger relay for pump & valve
+#define VOLTAGESENSORTYPE HIGH     // BREWDETECTION 3 configuration
+#define PINMODEVOLTAGESENSOR INPUT // Mode INPUT_PULLUP, INPUT or INPUT_PULLDOWN_16 (Only Pin 16)
+#define PRESSURESENSOR 0           // 1 = pressure sensor connected to A0; PINBREWSWITCH must be set to the connected input!
+#define STATUSLED 2                // 1 = set led pin high when brew or steam set point is within range otherwise low; 2 = vice versa
+
+// Brew Scale
+#define SCALE_SAMPLES 2                     // Load cell sample rate
+#define SCALE_CALIBRATION_FACTOR 3195.83    // Raw data is divided by this value to convert to readable data
+
+/* Pressure sensor
+ *
+ * measure and verify "offset" value, should be 10% of ADC bit reading @supply volate (3.3V)
+ * same goes for "fullScale", should be 90%
+ */
+#define OFFSET      102            // 10% of ADC input @3.3V supply = 102
+#define FULLSCALE   922            // 90% of ADC input @3.3V supply = 922
+#define MAXPRESSURE 200
+
+// PlatformIO OTA
+#define OTA true                   // true = OTA activated, false = OTA deactivated
+#define OTAHOST "silvia"           // Name to be shown in Arduino IDE/PlatformIO port
+#define OTAPASS "otapass"          // Password for OTA updates
+
+// MQTT
+#define MQTT 1                             // 1 = MQTT enabled, 0 = MQTT disabled
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
+#define MQTT_TOPIC_PREFIX ""              // topic will be "<MQTT_TOPIC_PREFIX><HOSTNAME>/<READING>"
+#define MQTT_SERVER_IP "192.168.178.9"   // IP-Address of locally installed mqtt server
+#define MQTT_SERVER_PORT 1883
+
+// INFLUXDB
+#define INFLUXDB 0                 // 1 = INFLUX enabled, 0 = INFLUX disabled
+#define INFLUXDB_URL ""            // InfluxDB server address
+#define INFLUXDB_AUTH_TYPE 1       // 1 = API Token , 2 = User/Pass
+#define INFLUXDB_API_TOKEN ""
+#define INFLUXDB_ORG_NAME ""
+#define INFLUXDB_USER ""
+#define INFLUXDB_PASSWORD ""
+#define INFLUXDB_DB_NAME "coffee"  // InfluxDB bucket name
+#define INFLUXDB_INTERVAL 5000     // Send interval in milliseconds
+
+// PID Parameters (not yet in Web interface)
+#define EMA_FACTOR 0.6             // Smoothing of input that is used for Tv (derivative component of PID). Smaller means less smoothing but also less delay, 0 means no filtering
+#define BREWPID_DELAY 10           // delay until enabling PID controller during brew (no heating during this time)
+
+// Backflush values
+#define FILLTIME 3000              // time in ms the pump is running
+#define FLUSHTIME 6000             // time in ms the 3-way valve is open -> backflush
+#define MAXFLUSHCYCLES 10           // number of cycles the backflush should run, 0 = disabled
+
+#define TEMPSENSOR 2               // Temp sensor type: 1 = DS18B20, 2 = TSIC306
